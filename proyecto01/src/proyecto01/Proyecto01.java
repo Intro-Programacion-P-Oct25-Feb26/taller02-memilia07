bf5385308a5c8a9e624492cee579aa0cfd3d84e1
memilia07 <mariaemiliasolano43@gmail.com>
2025-10-16 11:29:49 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,5 +14,8 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");    }
+System.out.println("Ejemplo de Java\n" +
+"Manejado desde GitHub\n" +
+"Versionado a través de Git\n" +
+"Creado por <ubicar su usuario de github>");    }
 }
